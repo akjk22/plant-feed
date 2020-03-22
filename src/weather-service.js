8ad829c9134ad9ef1e9ff0e7cb980d8a59354318
Andrew Kim <andrewswkim47@gmail.com>
2020-03-22 14:25:17 -0700

    goes through loop and displays all movie reviews

--- src/weather-service.js
+++ src/weather-service.js
@@ -3,10 +3,10 @@ import './weather-ui.js';
 
 // async/await method for api call
 export class MovieTitles {
-	async getMovieByTitle(lebowski) {
+	async getMovieByTitle() {
 		try {
 			let response = await fetch(
-				`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=lebowski&api-key=HgiG7fFZruJfUP9e5UItxW0veS1OgG0t`
+				`https://api.nytimes.com/svc/movies/v2/reviews/search.json?&api-key=HgiG7fFZruJfUP9e5UItxW0veS1OgG0t`
 			);
 			let jsonifiedResponse;
 			if (response.ok && response.status == 200) {
@@ -20,22 +20,3 @@ export class MovieTitles {
 		}
 	}
 }
-
-// Fetch method for API calling
-
-// fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`)
-// 	.then(function(response) {
-// 		if (response.ok && response.status == 200) {
-// 			return response.json();
-// 		} else {
-// 			return false;
-// 		}
-// 	})
-// 	.catch(function(error) {
-// 		return false;
-// 	})
-// 	.then(function(jsonifiedResponse) {
-// 		getElements(jsonifiedResponse);
-
-// 		});
-// });
--- src/weather-ui.js
+++ src/weather-ui.js
@@ -1,5 +1,6 @@
 import { MovieTitles } from './weather-service.js';
 
+
 $(document).ready(function() {
 	$('#weatherLocation').click(function() {
 		const lebowski = $('#location').val();
@@ -9,17 +10,27 @@ $(document).ready(function() {
 			let movieTitles = new MovieTitles();
 			const response = await movieTitles.getMovieByTitle(lebowski);
 			console.log(response);
-			getElements(response);
+			// getElements(response);
+			displayResults(response);
 	})();
 
 
-		const getElements = function(response) {
-			if (response) {
+	// 	const getElements = function(response) {
+
+	// 		if (response) {
+	// 			$('.showHumidity').append(
+	// 				`<div><h1>${response.results[0].display_title}</h1><h4>${response.results[0].byline}<p>${response.results[0]
+	// 						.link.url}</p><p>${response.results[0]
+	// 							.opening_date}</p></div>`);
+	// 	}
+	// };
+	const displayResults = (response) => {
+		for (let i = 0; i < response.results.length; i++) {
 				$('.showHumidity').append(
-					`<div><h1>${response.results[0].display_title}</h1><h4>${response.results[0].byline}<p>${response.results[0]
-							.link.url}</p><p>${response.results[0]
-								.opening_date}</p></div>`)
+						`<div><h1>#${i} ${response.results[i].display_title}</h1><h4>${response.results[i].byline}<p>${response.results[i]
+								.link.url}</p> </div>`
+				);
 		}
-	}
+};
 	});
 });

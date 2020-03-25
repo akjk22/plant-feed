10086ec2415a52d2ae619f7ea7765d3a1e3cfeee
Andrew Kim <andrewswkim47@gmail.com>
2020-03-25 16:35:55 -0700

    progress. showing results being returned in a dynamically populated drop down menu.

--- src/meal-ui.js
+++ src/meal-ui.js
@@ -1,9 +1,21 @@
 import { MealSearch } from './meal-service.js';
 
-
 $(document).ready(function() {
 	$('#meal-button').click(function() {
 		const meals = $('#meal-input').val();
+		// let dropdown = $('#dropdown');
+
+		// dropdown.empty();
+		// dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
+		// dropdown.prop('selectedIndex', 0);
+
+		// const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s';
+
+		// $.getJSON(url, function (data) {
+		// 	$.each(data, function (key, entry) {
+		// 		dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
+		// 	});
+		// });
 		// $('#location').val('');
 
 		// const state = $('#').val();
@@ -21,14 +33,41 @@ $(document).ready(function() {
 	// 				`<div><h1>${meal}</h1><p>${meal}</p></div>`);
 	// 	}
 	// };
-	const displayResults = (response) => {
 
+
+
+	const displayResults = (response) => {
+		// const options = response.map(meals => `
+		// <option value ='${meals}'>'${meals}'</option>`).join('');
+		// select.innerHTML = options;
+		let option;
 		// console.log(response.meals[5].strMeal);
 		for (let i = 0; i < response.meals.length; i++) {
-				$('.showHumidity').append(
-						`<div><h1>#${i} ${response.meals[i].strMeal}</h1></div>`
+			option = document.createElement('option');
+			option.text = response.meals[i].strMeal;
+			option.value = response.meals[i].strMeal;
+			dropdown.add(option);
+				// if (response.meals.strMeal) 
+				$('.showMealName').append(
+						`<div><h1> ${response.meals[i].strMeal}</h1></div>`
 				);
 		}
 };
+
 	});
+
+	// function generateList(meals) {
+
+	// 	const options= meals.map(meals => `
+		
+	// 	// call join on map to return values without commas
+		
+	// 	<option value='${meals}'>'${meals}'</option>
+		
+	// 	`).join('');
+		
+	// 	// insert option elements into the page
+		
+	// 	select.innerHTML = options;
+	// }
 });

4a80d0319930e69beddd27b896ddef886432f93d
Andrew Kim <andrewswkim47@gmail.com>
2020-03-27 00:02:15 -0700

    add images, youtube link, and css styles.

--- src/meal-ui.js
+++ src/meal-ui.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
     const displayResults = (response) => {
       for (let i = 0; i < response.meals.length; i++) {
-     		$('.recipe').append(`<div><h1> ${response.meals[i].strInstructions}</h1></div>`
+     		$('.recipe').append(`<div><h1> ${response.meals[i].strInstructions}</h1><img src="${response.meals[i].strMealThumb}" alt="" /><p>Follow Along on <a href="${response.meals[i].strYoutube}" style="text-decoration: underline; color: black;">YouTube</a></p></div>`
          )}
   };
   });
